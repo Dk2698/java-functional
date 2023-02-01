ea680bc3b69b521034a17d479280f078394d82df
DK2698 <dileepkumar1823@gmail.com>
2023-02-01 23:59:57 +0530

    Callbacks Like Javascript

---
+++ src/main/java/org/example/combinationpattern/CustomerValidatorService.java
@@ -0,0 +1,29 @@
+package org.example.combinationpattern;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CustomerValidatorService {
+    private boolean isEmailValid(String email){
+        return  email.contains("@");
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.startsWith("+91");
+    }
+
+    private boolean isAdult(LocalDate dob){
+        return  Period.between(dob, LocalDate.now()).getYears() > 16;
+    }
+
+    public  boolean isValid(Customer customer){
+        return isEmailValid(customer.getEmail()) && isPhoneNumberValid(customer.getPhoneNumber())
+                && isAdult(customer.getDob());
+    }
+
+//    public  boolean isValidPhone(Customer customer){
+//        return isEmailValid(customer.getEmail()) && isPhoneNumberValid(customer.getPhoneNumber())
+//                && isAdult(customer.getDob());
+//    }
+
+}

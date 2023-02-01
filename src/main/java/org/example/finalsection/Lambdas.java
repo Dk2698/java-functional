0517b4c814a22c6253ad7911da5b09029f142dcf
DK2698 <dileepkumar1823@gmail.com>
2023-02-02 00:08:39 +0530

    Lambdas

---
+++ src/main/java/org/example/finalsection/Lambdas.java
@@ -0,0 +1,25 @@
+package org.example.finalsection;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Lambdas {
+    public static void main(String[] args) {
+        Function<String, String>  uppercaseName= name -> name.toUpperCase();
+
+        Function<String, String>  uppercaseName1= name ->{
+            // logic
+            if(name.isBlank()) throw  new IllegalArgumentException("");
+            return name.toUpperCase();
+        };
+
+        BiFunction<String,Integer, String>  upperCaseName= (name, age) ->{
+            if(name.isBlank()) throw  new IllegalArgumentException("");
+            System.out.println(age);
+            return name.toUpperCase();
+        };
+
+        System.out.println(upperCaseName.apply("Alex0", 20));
+
+    }
+}

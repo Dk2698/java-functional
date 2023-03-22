4d1ba07c3cc1e3039993109b9199a6a878359adc
DK2698 <dileepkumar1823@gmail.com>
2023-03-23 00:52:43 +0530

    built method like function, intprediacte

--- src/main/java/org/example/lambda/Main.java
+++ src/main/java/org/example/lambda/Main.java
@@ -1,7 +1,9 @@
 package org.example.lambda;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.*;
 
 public class Main {
     private  int fields;
@@ -18,8 +20,26 @@ public class Main {
 //            PRICE++ ; // local variable must be final in  lambada expression
             return  hotel.getPricePerNight() <= PRICE;
         };
-        PRICE++; // change here outsize lambda expression
+       // PRICE++; // change here outsize lambda expression
         return  hotelService.filterHotels(lambdaExp);
+    }
+
+    public  FilteringCondition  testLambda1(){
+        HotelService hotelService = new HotelService();
+
+        int PRICE = 200; // primitive
+
+        List<Integer> arr = new ArrayList<>();
+        /// arr reference variable
+        FilteringCondition lambdaExp = hotel -> {
+            return  hotel.getPricePerNight() <= arr.size(); // value capture
+        };
+        // PRICE++ not change;
+
+        arr.add(1);
+//        arr = new ArrayList<>(); // new object change
+
+      return  lambdaExp;
     }
     public static void main(String[] args) {
         HotelService  hotelService = new HotelService();
@@ -59,6 +79,9 @@ public class Main {
         List<Hotel> hotels1 = hotelService.filterHotels(lambdaExp);
 
 
+        Main main = new Main();
+        FilteringCondition filteringCondition = main.testLambda1();
+        hotelService.filterHotels(filteringCondition);
     }
 
     /* passing function in java
@@ -71,4 +94,41 @@ public class Main {
     // in argument type remove and one parameter no need round parenthesis
     // one line statement, remove curly braces, return, semicolon
 
+    public  static  void  exampleFunction(){
+        Predicate<Integer> divisbleBy2 = (no) -> {
+            return  no %2 == 0;
+        };
+
+        Predicate<Integer> divisbleBy3 = no -> no % 3 == 0;
+        IntPredicate divisbleBy4 = no -> no % 4 == 0;
+
+        Predicate<Integer> divisableBy6 = divisbleBy2.and(divisbleBy3);
+        System.out.println(divisableBy6.test(6));
+
+
+        Consumer<Integer> consumer = a -> System.out.println(a);
+        consumer.accept(6);
+
+        List<Integer> list = List.of(1,2,3,4,4);
+        list.forEach(consumer);
+
+        Supplier<Double> supplier = () ->  Math.random();
+        System.out.println(supplier.get());
+
+        Function<String, Integer> strToLenMap = (string) -> string.length();
+
+
+        Function<String, String> identity = (string) -> string;
+        Function<String, String> identity1 = Function.identity();
+
+        Integer kumar = strToLenMap.apply("kumar");
+        System.out.println(kumar);
+
+
+        Function<Integer, Integer> squares  = a -> a * a;
+        Function<Integer, Integer> addOne  = a -> a + 1;
+        Integer num = squares.andThen(addOne).apply(6);
+        System.out.println(num);
+
+    }
 }
